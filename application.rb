a328777d8e6f983304118aa4b0f525b96e5920b2
theneubeck <jens.neubeck@gmail.com>
2010-02-15 23:51:07 +0100

    Transformed app to sinatra base

--- application.rb
+++ application.rb
@@ -1,22 +1,45 @@
+# encoding: utf-8
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'environment'
 
-configure do
+# basic setup
+class Sinatra::Base
+  register SinatraMore::MarkupPlugin
+  register SinatraMore::RenderPlugin
+
+  use Rack::Session::Cookie
+  use Rack::Flash, :sweep => true
+  use Rack::MethodOverride
+  
+  # use Rack::Session::Cookie, :secret=>SECRET
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set :static, true
   set :views, "#{File.dirname(__FILE__)}/views"
+  set :public, Proc.new { File.join(File.dirname(__FILE__), 'public') }
+  set :run, false
+
+  
+  def html_opts(hash)
+    hash.map{|k,v| "#{k}=\"#{h(v)}\""}.join(' ')
+  end
+  
+  helpers do
+    include ViewHelpers
+  end
+  
+  error do
+    e = request.env['sinatra.error']
+    Kernel.puts e.backtrace.join("\n")
+    'Application error'
+  end
+    
 end
 
-error do
-  e = request.env['sinatra.error']
-  Kernel.puts e.backtrace.join("\n")
-  'Application error'
-end
+# routing class
 
-helpers do
-  # add your helpers here
-end
-
-# root page
-get '/' do
-  haml :root
+class MainApp < Sinatra::Base
+  
+  register PlugableRouter
 end
