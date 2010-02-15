311795674e5b2934a6c5897eba041f3e31c2dfb7
theneubeck <jens.neubeck@gmail.com>
2010-02-15 23:50:49 +0100

    Updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'sinatra'
+require 'rack/test'
 require 'spec'
 require 'spec/interop/test'
-require 'rack/test'
-
 # set test environment
 Sinatra::Base.set :environment, :test
 Sinatra::Base.set :run, false
@@ -12,10 +11,6 @@ Sinatra::Base.set :logging, false
 
 require 'application'
 
-# establish in-memory database for testing
-DataMapper.setup(:default, "sqlite3::memory:")
-
 Spec::Runner.configure do |config|
-  # reset database before each example is run
-  config.before(:each) { DataMapper.auto_migrate! }
+  config.before(:each) { # }
 end
